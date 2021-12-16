25eb2748421e4c621affdf4ebdb7d0e2dd8aef93
Nicolas <nicolasabdoul@outlook.fr>
2021-12-16 23:30:07 +0100

    update services to load ressources

--- projects/invoices-builder/src/lib/form/customer-selector/customer-selector.component.ts
+++ projects/invoices-builder/src/lib/form/customer-selector/customer-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CustomerSelectorService } from '../customer-selector.service';
 
 @Component({
   selector: 'lib-customer-selector',
@@ -8,8 +9,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class CustomerSelectorComponent implements OnInit {
   @ViewChild('test') toggleButton!: ElementRef;
 
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private cs: CustomerSelectorService) {}
+
+  data!: {};
 
   selectMode = false;
   foods: any[] = [
@@ -17,4 +19,15 @@ export class CustomerSelectorComponent implements OnInit {
     { value: 'pizza-1', viewValue: 'Pizza' },
     { value: 'tacos-2', viewValue: 'Tacos' },
   ];
+
+  ngOnInit() {
+    console.log('data', this.data);
+    // this.cs.allObservable.subscribe((d) => (this.data = d));
+  }
+
+  onChange(event: any) {
+    console.log('e', event);
+    this.data = event;
+    this.cs.setUpdatedData(this.data);
+  }
 }
